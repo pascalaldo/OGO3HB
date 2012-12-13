0d8ff1719d6d9ca6be182efd32e86cb4ba9cbfc1
Lafayette de Windt <l.n.j.d.windt@student.tue.nl>
2012-12-13 16:24:08 +0100

    Complete method to determine tact (and tcycle)

---
+++ DetermineTime.m
@@ -0,0 +1,34 @@
+function [tcycle tact] = DetermineTime(hbt, hbp)
+%This function determines tcycle and tact bases on a selected pressure
+%curve (hbp) and it's corresponding time period (hbt).
+
+%% Determine tcylce solemnly by using hbt.
+tcycle = hbt(length(hbt)) - hbt(1);
+
+%% Determine tact defined by time between peaks of hbp by using hbp and hbt.
+%Determine the mean index of the first peak (ifp)
+ifp = mean(find(hbp == max(hbp)));
+
+%Select a part of hbp (phbp) without left side of first peak
+phbp = hbp(ifp:length(hbp));
+%Determine differentials of phbp (dphbp)
+dphbp = diff(phbp);
+%Determine index of maximum steap near second peak (msphbp)
+msphbp = mean(find(dphbp == max(dphbp)));
+
+%Select a part of phbp without right side of first peak 
+%(and a part of the left side of the second peak) (pphbp).
+pphbp = phbp(msphbp:length(phbp));
+%Determine the index of the second peak (impphbp)
+impphbp = mean(find(pphbp == max(pphbp)));
+%Determine the mean index of the first peak (isp)
+isp = round(impphbp + ifp + msphbp);
+
+tact = hbt(isp) - hbt(ifp) - hbt(1);
+
+%% Feedback mechanisme - Plotting result for checking
+hold on
+plot(hbt,hbp)
+plot(hbt(ifp),hbp(ifp),'r*')
+plot(hbt(isp),hbp(isp),'r*')
+plot(hbt(ifp:isp),hbp(ifp:isp),'r')
