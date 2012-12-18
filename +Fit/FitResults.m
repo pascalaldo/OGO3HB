0c47556aa701473b1d82aab9eddde265adbdda98
Pascal Pieters <pascalaldo@gmail.com>
2012-12-18 22:53:48 +0100

    Added all results

--- +Fit/FitResults.m
+++ +Fit/FitResults.m
@@ -3,10 +3,12 @@ global showplots;
 for i = 1:40
     showplots = false;
     filename = sprintf('Results\\%d_res1.mat',i);
-    if exist(filename,'file') == 2
+    if exist(filename,'file') == 2 && ~exist(sprintf('FitResults\\%d',i),'dir')
         mkdir(sprintf('FitResults\\%d',i));
         fprintf('Fitting patient %d\n',i);
         load(filename);
+        psz = size(hbp);
+        if psz(2) > psz(1), hbp = hbp'; end
         [x nipar] = Fit.FitModel(vt,v,hbt,hbp,i);
         showplots = true;
         Model.Circulation(x,nipar);
@@ -15,4 +17,3 @@ for i = 1:40
     end
 end
 end
-
