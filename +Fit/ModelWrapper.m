998f5f580d0c2856e037a25579bb166db6c6adaf
Pascal Pieters <pascalaldo@gmail.com>
2012-12-18 13:04:21 +0100

    More fitting

--- +Fit/ModelWrapper.m
+++ +Fit/ModelWrapper.m
@@ -2,12 +2,19 @@ function [d dp dv] = ModelWrapper(vt, v, hbt, hbp, par, nipar)
 %MODELWRAPPER Summary of this function goes here
 %   Detailed explanation goes here
 
-[mt mpart mvlv] = Model.Circulation(par,nipar);
+[mt mpart mvlv mvlvtemp mplv] = Model.Circulation(par,nipar);
 %[mt mpart mvlv] = funccirc(par,nipar);
 
-% Compare pressure
+% Compare arterial pressure
 ipart = interp1(mt,mpart,hbt,'linear','extrap');
 dp = hbp'-ipart;
+
+% Compare lv pressure
+%[temp mint] = BloodPressure.DeterminePeak(hbt,hbp);
+%lastindex = find(hbt>mint,1,'first');
+%iplv = interp1(mt,mplv,hbt(1:lastindex),'linear','extrap');
+%dplv = hbp(1:lastindex)'-iplv;
+%figure; plot(hbt(1:lastindex),hbp(1:lastindex),'b-',hbt(1:lastindex),iplv,'r-',hbt,ipart,'g:');
 
 % Compare volume
 vtimeshift = par(end);
@@ -16,9 +23,11 @@ ivlv = interp1(mt,mvlv,vt,'linear','extrap');
 dv = v-ivlv;
 
 %Join dp and dv
-factor = length(dp)/length(dv);
-dv = dv.*factor;
-d = [dp';dv];
+factor1 = length(dp)/length(dv);
+dv = dv.*factor1;
+%factor2 = length(dp)/length(dplv);
+%dplv = dplv.*factor2;
+d = [dv;dp']; %;dplv'
 
 %% Plotting
 %{
